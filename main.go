f714b3bebd3ea71797d6c03d1d6d95b5a72e0147
Lucas Kacher <promiesofcake@gmail.com>
2015-09-02 20:16:34 -0700

    Add some work

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/gogits/git"
+	"os/exec"
 )
 
 // var cwd string
@@ -15,10 +13,10 @@ func main() {
 	wd, _ := os.Getwd()
 	fmt.Println(wd)
 
-	r, err := git.OpenRepository(wd)
-	if err != nil {
-		log.Fatal("Could not open repository")
-	}
+	// r, err := git.OpenRepository(wd)
+	// if err != nil {
+	// 	log.Fatal("Could not open repository")
+	// }
 
 	a := os.Args
 
@@ -29,17 +27,25 @@ func main() {
 
 	fmt.Println("arguments:", a)
 
-	refA, err := r.GetCommit(a[1])
-	if err != nil {
-		log.Fatal(a[1] + "not a valid hash")
-	}
-	refB, err := r.GetCommit(a[2])
-	if err != nil {
-		log.Fatal(a[2] + "not a valid hash")
-	}
+	gitLogArgs := fmt.Sprintf("log --merges --grep=\"Merge pull request\" --pretty=format:\"%s\" %s..%s", "%s", a[1], a[2])
+	fmt.Println(gitLogArgs)
 
-	fmt.Println(refA.Message())
-	fmt.Println(refB.Message())
+	c := exec.Command("git", gitLogArgs)
+	output, _ := c.CombinedOutput()
+
+	fmt.Println(string(output))
+
+	// refA, err := r.GetCommit(a[1])
+	// if err != nil {
+	// 	log.Fatal(a[1] + "not a valid hash")
+	// }
+	// refB, err := r.GetCommit(a[2])
+	// if err != nil {
+	// 	log.Fatal(a[2] + "not a valid hash")
+	// }
+
+	// fmt.Println(refA.Message())
+	// fmt.Println(refB.Message())
 }
 
 const usage = `
