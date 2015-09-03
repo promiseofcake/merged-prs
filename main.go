ba6349931fe479474f0430b499e73062771d3e38
Lucas Kacher <lucas@vsco.co>
2015-09-02 18:40:21 -0700

    testing with git packages

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/gogits/git"
 )
 
-var cwd string
+// var cwd string
 
 func main() {
 
-	cwd = getCwd()
-	fmt.Println(cwd)
+	wd, _ := os.Getwd()
+	fmt.Println(wd)
+
+	r, err := git.OpenRepository(wd)
+	if err != nil {
+		log.Fatal("Could not open repository")
+	}
 
 	a := os.Args
 
@@ -21,6 +29,17 @@ func main() {
 
 	fmt.Println("arguments:", a)
 
+	refA, err := r.GetCommit(a[1])
+	if err != nil {
+		log.Fatal(a[1] + "not a valid hash")
+	}
+	refB, err := r.GetCommit(a[2])
+	if err != nil {
+		log.Fatal(a[2] + "not a valid hash")
+	}
+
+	fmt.Println(refA.Message())
+	fmt.Println(refB.Message())
 }
 
 const usage = `
@@ -29,8 +48,4 @@ merged-prs <HASH> <HASH>
 
 func showUsage() {
 	fmt.Println(usage)
-}
-
-func getCwd() string {
-	return os.Getenv("PWD")
 }
