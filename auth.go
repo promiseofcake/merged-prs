7e76218cf6c68050911bc36c0530a59bc99a6de5
Lucas Kacher <lucas@vsco.co>
2019-05-17 17:02:15 -0700

    feat: modernize on HEAD for deps

--- auth.go
+++ auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
@@ -9,6 +10,6 @@ func authWithGitHub(tkn string) *github.Client {
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: tkn},
 	)
-	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	tc := oauth2.NewClient(context.TODO(), ts)
 	return github.NewClient(tc)
 }
